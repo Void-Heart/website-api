bad8872dd86552c4f442efdd63f91026f5d43919
LukeG007 <luke@gilsinger.net>
2022-06-06 00:49:58 -0400

    Add functionality to get commit history from github

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 import errors
+import commit_history_mgmt
 
 app = Flask(__name__)
 api = Api(app, errors=errors.errors, catch_all_404s=True)
-
+commit_history = commit_history_mgmt.CommitHistory()
+commit_history.update_data()
 class GetCommitHistory(Resource):
     def get(self):
-        data = [{'message': 'test'}, {'message': 'test2'}, {'message': 'test3'}]
+        parser = reqparse.RequestParser()
+        parser.add_argument('page', type=int)
+        args = parser.parse_args()
+        page = args['page']
+        if page is None:
+            page = 1
+        data = {
+            'page_count': commit_history.page_count,
+            'commit_count': commit_history.commit_count,
+            'commit_data': commit_history.commit_data
+        }
         return data
 
+class UpdateStoredCommitHistory(Resource):
+    def get(self):
+        commit_history.update_data()
+
 api.add_resource(GetCommitHistory, '/get_commit_history')
+api.add_resource(UpdateStoredCommitHistory, '/update_stored_commit_history')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5636)
---
+++ update_stored_history.py
@@ -0,0 +1,38 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+import json
+
+def get_data(key):
+    reached_last_page = False
+    page_count = 1
+    commit_count = 0
+    commit_data = []
+    while not reached_last_page:
+        data_ = requests.get('https://api.github.com/repos/{}/commits?page={}'.format(key[0], page_count), auth=HTTPBasicAuth(key[1], key[2])).json()
+        data = []
+        for commit in data_:
+            data.append(commit['commit'])
+        commit_data += data
+        commit_count += len(data)
+        if len(data) < 30:
+            reached_last_page = True
+        else:
+            page_count += 1
+    return page_count, commit_count, commit_data
+
+keyf = open('key', 'r')
+key = keyf.read().split(':')
+keyf.close()
+page_count, commit_count, commit_data = get_data(key)
+os.remove('stored_history.json')
+historyf = open('stored_history.json', 'x')
+history_data = {
+    'page_count': page_count,
+    'commit_count': commit_count,
+    'commit_data': commit_data
+}
+
+historyf.write(json.dumps(history_data))
+historyf.close()
+requests.get('http://localhost:5636/update_stored_commit_history')
