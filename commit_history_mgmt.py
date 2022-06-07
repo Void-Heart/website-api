a006250493b989ec13f7c3cd2e655dcf00a2312c
LukeG007 <luke@gilsinger.net>
2022-06-07 00:05:53 -0400

    WHY

--- commit_history_mgmt.py
+++ commit_history_mgmt.py
@@ -3,7 +3,6 @@ import restore_history
 class CommitHistory:
     def __init__(self):
         self.commit_data = []
-        self.page_count = 1
         self.commit_count = 0
         restore_history.restore()
     def update_data(self):
@@ -11,6 +10,5 @@ class CommitHistory:
         data = f.read()
         f.close()
         data = json.loads(data)
-        self.page_count = data['page_count']
         self.commit_count = data['commit_count']
         self.commit_data = data['commit_data']
