6eda055c276d75a2b9237ef3e2ef2f5fdc6bd07c
LukeG007 <luke@gilsinger.net>
2022-06-07 00:03:47 -0400

    Remove more paging stuff (why did i do this)

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ commit_history = commit_history_mgmt.CommitHistory()
 commit_history.update_data()
 class GetCommitHistory(Resource):
     def get(self):
-        if page is None:
-            page = 1
         data = {
             'commit_count': commit_history.commit_count,
             'commit_data': commit_history.commit_data
