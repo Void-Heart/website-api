86976d2fb926e87a9a82f5f5e104989897b031c4
LukeG007 <luke@gilsinger.net>
2022-06-07 00:05:13 -0400

    More paging

--- restore_history.py
+++ restore_history.py
@@ -3,6 +3,6 @@ def restore():
     if not os.path.exists('stored_history.json'):
         historyf = open('stored_history.json', 'x')
         historyf.write(
-            '{"page_count": 1, "commit_count": 0, "commit_data": []}'
+            '{"commit_count": 0, "commit_data": []}'
         )
         historyf.close()
